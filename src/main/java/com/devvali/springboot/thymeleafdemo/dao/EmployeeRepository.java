2aed82a374c5939dd9c9a7ce4495feb1cc59447a
Valnetin Iliescu <iliescuvalentin8@gmail.com>
2023-07-06 10:46:18 +0300

    Initial commit

---
+++ src/main/java/com/devvali/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.devvali.springboot.thymeleafdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devvali.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+	
+}
---
+++ src/main/java/com/devvali/springboot/thymeleafdemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.devvali.springboot.thymeleafdemo.service;
+
+import java.util.List;
+
+import com.devvali.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeService {
+
+	List<Employee> findAll();
+	
+	Employee findById(int theId);
+	
+	void save(Employee theEmployee);
+	
+	void deleteById(int theId);
+	
+}
